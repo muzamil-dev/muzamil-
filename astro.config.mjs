5ddf9e0dcb7740e988a1475ee56743e228e8fa72
Muzamil <muzamil@MacBook-Pro.local>
2025-05-07 22:01:15 -0400

    deployment fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
   vite: {
-    resolve: {
-      alias: {
-        '@/': new URL('./src/', import.meta.url).pathname
-      }
-    }
+    plugins: [tailwindcss()]
   }
 });
